01b7fff697307d76a3c65dab1ab9a84dd87f528b
HELLOHIDI <fbgmlwo123@naver.com>
2023-07-08 14:16:22 +0900

    [Add] UIFont 추가 #7

--- PPPCLUB-iOS/Global/Extension/UIFont+.swift
+++ PPPCLUB-iOS/Global/Extension/UIFont+.swift
@@ -10,4 +10,31 @@ import UIKit
 //MARK: - Custom Font
 
 extension UIFont{
+    
+    class var pppTitle1: UIFont {
+        return UIFont(name: "BioRhyme-Regular", size: 24)!
+    }
+    
+    class var pppTitle2: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 24)!
+    }
+    
+    class var pppBody1: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 20)!
+    }
+    
+    class var pppBody2: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 16)!
+    }
+    
+    class var pppCaption1: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 14)!
+    }
+    
+    class var pppCaption2: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 12)!
+    }
+    
+    
 }
+
