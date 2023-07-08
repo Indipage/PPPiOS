5de117d4b9c10d8f73c555d53a7b917db43fe979
HELLOHIDI <fbgmlwo123@naver.com>
2023-07-08 14:05:23 +0900

    [Add] 폰트 추가 #7

---
+++ PPPCLUB-iOS/Presentation/Detail/View/DetailView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailView.swift
+//  PPPCLUB-iOS
+//
+//  Created by 류희재 on 2023/07/08.
+//
+
+import Foundation
---
+++ PPPCLUB-iOS/Presentation/Detail/ViewController/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  PPPCLUB-iOS
+//
+//  Created by 류희재 on 2023/07/08.
+//
+
+import UIKit
+
+final class DetailViewController: BaseViewController {
+    override func viewDidLoad() {
+        self.view.backgroundColor = .green
+    }
+}
