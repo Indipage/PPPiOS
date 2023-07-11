8ca8609437d2617f57726449081eff0701c20e0e
HELLOHIDI <fbgmlwo123@naver.com>
2023-07-12 06:51:52 +0900

    [Feat] Image 에셋 추가 #22

--- PPPCLUB-iOS/Global/Constant/Image.swift
+++ PPPCLUB-iOS/Global/Constant/Image.swift
@@ -5,4 +5,28 @@
 //  Created by 류희재 on 2023/07/03.
 //
 
-import Foundation
+import UIKit
+
+public enum Image {
+    //MARK: - Logo
+    
+    static let logoAppIcon = UIImage(named: "logo_app_icon")!
+    static let logoCombination = UIImage(named: "logo_combination")!
+    static let logoSymbol = UIImage(named: "logo_symbol")!
+    
+    //MARK: - Icon
+    
+    static let arrowDown = UIImage(named: "back")
+    static let bookmark = UIImage(named: "bookmark")!
+    static let bookmarkFill = UIImage(named: "bookmark.fill")!
+    static let forward = UIImage(named: "forward")!
+    static let ticket = UIImage(named: "ticket")!
+    
+    //MARK: - Mock
+    
+    static let mockArticle = UIImage(named: "mockArticle")!
+    static let mockArticleCard = UIImage(named: "mockArticleCard")!
+    static let mockArticleCardPacked = UIImage(named: "mockArticleCardPacked")!
+    static let mockBook = UIImage(named: "mockBook")!
+    static let mockBookStore = UIImage(named: "mockBookStore")!
+}
