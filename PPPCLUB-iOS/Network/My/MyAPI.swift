aaf40199288a53518e93a30ef2f74847794483a0
HELLOHIDI <fbgmlwo123@naver.com>
2023-07-15 20:11:34 +0900

    [Feat] 유저 정보 불러오기 통신함수 구현 #51

--- PPPCLUB-iOS/Network/My/MyAPI.swift
+++ PPPCLUB-iOS/Network/My/MyAPI.swift
@@ -5,4 +5,24 @@
 //  Created by 류희재 on 2023/07/15.
 //
 
-import Foundation
+import UIKit
+
+import Moya
+
+final class MyAPI: BaseAPI {
+    static let shared = MyAPI()
+    private var myProvider = MoyaProvider<MyService>(plugins: [MoyaLoggingPlugin()])
+    private override init() {}
+}
+
+extension MyAPI{
+    public func getMyInfo(completion: @escaping (NetworkResult<Any>) -> Void) {
+        myProvider.request(.getMyInfo) { (result) in
+            self.disposeNetwork(
+                result,
+                dataModel: MyUserInfoResult.self,
+                completion: completion
+            )
+        }
+    }
+}
