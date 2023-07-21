f563fd21b77fb8eb8922f8df521a0a1ed59a32f0
shinzz410 <tlswldnjs410@gmail.com>
2023-07-21 21:14:04 +0900

    [Fix] ColletionView 버튼 위에서 스크롤 되도록 수정 #99

--- PPPCLUB-iOS/Presentation/Home/View/HomeWeeklySlidedView.swift
+++ PPPCLUB-iOS/Presentation/Home/View/HomeWeeklySlidedView.swift
@@ -16,6 +16,13 @@ class HomeWeeklySlidedView: UICollectionView {
         style()
     }
     
+    override func touchesShouldCancel(in view: UIView) -> Bool {
+        if view is UIButton {
+            return true
+        }
+        return super.touchesShouldCancel(in: view)
+    }
+    
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
